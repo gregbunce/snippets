7e598e7134a0902444e02ef09e224615f87a6d70
Greg Bunce <gbunce@utah.gov>
2018-05-04 15:58:23 -0600

    update to use new functions

--- python/snippets_utransETL.py
+++ python/snippets_utransETL.py
@@ -1,44 +1,25 @@
-# python snippits for use in the utran ETL project
+## To validate fields with Domain values use the appropriate functions, such as....                
+ValidateAssign_POSTTYPE(row, row.S_TYPE, countyNumber)
+ValidateAssign_DOT_FCLASS(row, row.S_AGFUNC, countyNumber) 
+ValidateAssign_DOT_SRFTYP(row, row.S_SURF, countyNumber)
 
 
-#### BEGIN >> CHECK FOR VALID DOMAIN VALUES ####
-# POSTTYPE #
-postTypeDomain = GetCodedDomainValue(row.S_TYPE, dictOfValidPostTypes)
-if postTypeDomain != "":
-    row.POSTTYPE = postTypeDomain
-elif postTypeDomain == "" and len(row.S_TYPE) > 1:  
-    # add the post type they gave to the notes field so we can evaluate it
-    row.UTRANS_NOTES = row.UTRANS_NOTES + "POSTTYPE: " + row.S_TYPE + "; "
-    # add the bad domain value to the text file log
-    AddBadValueToTextFile(countyNumber, "POSTTYPE", str(row.S_TYPE))
+## To parse out full address and check for validated and parsed values, call the function this way...
+if row.ALIAS1 is not None or row.ALIAS1 != "":
+    is_valid_parse, pre_dir, street_name, post_type, post_dir = ParseFullAddress(row.ALIAS1)
 
-# CLASS #
-classDomain = GetCodedDomainValue(row.CLASS, dictOfValidRoadClass)
-if classDomain != "":
-    row.DOT_FCLASS = classDomain
-elif classDomain == "" and row.CLASS is not None:
-    if not row.CLASS.isspace():
-        # add the CLASS they gave to the notes field so we can evaluate it
-        row.UTRANS_NOTES = row.UTRANS_NOTES + "DOT_FCLASS: " + row.CLASS + "; "
-        # add the bad domain value to the text file log
-        AddBadValueToTextFile(countyNumber, "DOT_FCLASS", str(row.CLASS))
-
-#### CHECK FOR VALID DOMAIN VALUES << END ####        
-        
-        
-        
-#### BEGIN >>> CALL VALIDATE FUNCTIONS ####
-## AN_NAME #
-if row.ACS_NAME != "":
-    # call the validation function
-    an_Name, an_PostDir = Validate_AN_NAME(row.ACS_NAME)
-    # AN_NAME
-    row.AN_NAME = an_Name            
-
-    # AN_POSTDIR
-    if an_PostDir != "":
-        row.AN_POSTDIR = an_PostDir
+    if is_valid_parse == True:
+        # it WAS a valid parse
+        if street_name.isdigit():
+            # the street name is numeric
+            row.AN_NAME = street_name
+            row.AN_POSTDIR = post_dir
+        else:
+            # the streetname is alpha
+            row.A1_PREDIR = pre_dir
+            row.A1_NAME = street_name
+            row.A1_POSTTYPE = post_type
+            row.A1_POSTDIR = post_dir
     else:
-        row.AN_POSTDIR = row.ACS_SUF
-                    
-#### CALL VALIDATE FUNCTIONS << END ####                    
+        # it was NOT a valid parse
+        row.A1_NAME = row.ALIAS1
