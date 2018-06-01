65fd1534f68ec94c37d5b0073887198eb6129e3e
Greg Bunce <gbunce@utah.gov>
2018-06-01 16:20:41 -0600

    add a few other helper function snippets

--- python/snippets_utransETL.py
+++ python/snippets_utransETL.py
@@ -6,5 +6,15 @@ ValidateAndAssign_FieldValue(row, "DOT_SRFTYP", row.SURFTYPE, countyNumber, dict
 ValidateAndAssign_FieldValue(row, "STATUS", row.STATUS_, countyNumber, dictOfValidStatus)
 
 ## To parse out full address and check for validated and parsed values, call the function this way...
-# ParseAndAssign_FullAddress(row, field_name_to_parse, bool_primary=False, bool_alias1=False, bool_alias2=False)
+# ParseAndAssign_FullAdds(row, field_name_to_parse, bool_primary=False, bool_alias1=False, bool_alias2=False)
 ParseAndAssign_FullAddress(row, "ALIAS1", False, True, False)
+
+## other helper functions
+HasFieldValue(field_value):
+  """ example: (row.STATUS) """
+    
+VertLevel_TranslateOldDomainToNewDomain(row, old_vert_domain_value, county_number):
+  """ example: (row, row.VERTLEVEL, countyNumber) """
+  
+HasValidDirection(field_value):
+  """ example: (row.STATUS) """
