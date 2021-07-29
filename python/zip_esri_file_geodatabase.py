6b9d40f8a0b5fa853b54d16a94422997b428f76a
Greg Bunce <gbunce@utah.gov>
2021-07-29 16:22:04 -0600

    use fstring

--- python/zip_esri_file_geodatabase.py
+++ python/zip_esri_file_geodatabase.py
@@ -2,7 +2,7 @@ import os, zipfile
 
 #: This function zips an esri file geodatabase using python 3 (note: full_path_to_fgdb must include the .gdb file extention).
 def zip_fgdb(full_path_to_fgdb):
-    outFile = full_path_to_fgdb[0:-4]+'_gdb.zip'
+    output = f'{full_path_to_fgdb[0:-4]}_gdb.zip'
     gdbName = os.path.basename(full_path_to_fgdb)
 
     with zipfile.ZipFile(outFile,mode='w',compression=zipfile.ZIP_DEFLATED,allowZip64=True) as myzip:
